441d9f94c50c265bd92d5e84fb08e508dee5f646
reszkojr <s.reszkojr@gmail.com>
2023-09-29 09:03:59 -0300

    "hello, username" on login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
 
 const Header = () => {
+	const { user } = useContext(AuthContext);
 	return (
 		<div>
 			<Link to='/'>Home</Link>
@@ -14,6 +17,7 @@ const Header = () => {
 			<Link to='/login'>Login</Link>
 			<span> | </span>
 			<Link to='/signup'>Sign up</Link>
+			{user && <p>Hello, {user.username}!</p>}
 		</div>
 	);
 };
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,19 @@
 import { createContext, useState, useEffect, FormEvent } from 'react';
 import Props from '../utils/Props';
 import axios, { AxiosPromise, AxiosResponse } from 'axios';
+import jwt_decode from 'jwt-decode';
+
+type User = {
+	exp: number;
+	iat: number;
+	jti: string;
+	token_type: string;
+	user_id: 4;
+	username: string;
+};
 
 const AuthContext = createContext({
+	user: {} as User | null,
 	loginUser: async (e: FormEvent<HTMLFormElement>) => {},
 });
 
@@ -27,11 +38,14 @@ export const AuthProvider = ({ children }: Props) => {
 					},
 				}
 			);
-			console.log(response.data);
 			switch (response.status) {
-				case 201:
-					console.log('Account succesfully created! Welcome to Manthano!');
+				case 200:
+					alert('Welcome to Manthano!');
+					setAuthTokens(response.data);
+					setUser(jwt_decode(response.data.access));
 					break;
+				default:
+					alert('Something wrong is not right.');
 			}
 		} catch (error: unknown) {
 			if (axios.isAxiosError(error)) {
@@ -49,6 +63,7 @@ export const AuthProvider = ({ children }: Props) => {
 	};
 
 	let contextData = {
+		user: user,
 		loginUser: loginUser,
 	};
 
