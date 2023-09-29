0e9d2acfc37e4f1db92f28a2d83adbf18fb2762c
reszkojr <s.reszkojr@gmail.com>
2023-09-29 03:48:13 -0300

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+
 const App = () => {
 	return (
 		<>
-         <p>Hello world!</p>
+			<Router>
+				<Routes>
+					<Route path='/' element={<LandingPage />} />
+					<Route path='/login' element={<LoginPage />} />
+				</Routes>
+			</Router>
 		</>
 	);
 };
