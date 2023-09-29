3f2ccad0c4805f5d8567b1b725a5c5c2f5eaa4a4
reszkojr <s.reszkojr@gmail.com>
2023-09-29 03:40:10 -0300

    login page

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <form>
+            <input type='text' name='username' placeholder='cronaldo7' />
+            <input type='password' name='password' placeholder='messisucks' />
+            <input type="submit" />
+        </form>
+    </div>
+  )
+}
+
+export default LoginPage;
