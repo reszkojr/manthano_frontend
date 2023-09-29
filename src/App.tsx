30f7d18b6a4b74ef78857e717cd6880e86074df6
reszkojr <s.reszkojr@gmail.com>
2023-09-29 03:51:55 -0300

    header links

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
+import Header from './components/Header';
 
 const App = () => {
 	return (
 		<>
 			<Router>
+				<Header />
 				<Routes>
 					<Route path='/' element={<LandingPage />} />
 					<Route path='/login' element={<LoginPage />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+	return (
+		<div>
+			<Link to='/'>Home</Link>
+			<span> | </span>
+			<Link to='/classroom'>Classroom</Link>
+			<span> | </span>
+			<Link to='/faq'>FAQ</Link>
+			<span> | </span>
+			<Link to='/contribute'>Contribute</Link>
+			<span> | </span>
+			<Link to='/login'>Login</Link>
+		</div>
+	);
+};
+
+export default Header;
