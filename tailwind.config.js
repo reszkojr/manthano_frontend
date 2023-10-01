35f9f00057e93e9b7d6d140c6722f9db04b23559
reszkojr <s.reszkojr@gmail.com>
2023-09-30 22:45:03 -0300

    added color palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#303841',
+        'secondary': '#1F2328',
+        'contrast': '#3A4750',
+        'yellow': '#FF8200',
+        'blue': '#3880D9',
+        'white': '#EEEEEE',
+      }
+    },
   },
   plugins: [],
 }
