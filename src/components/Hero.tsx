ef3a953f9830130d36688eecc42d9b8d00fbfd29
reszkojr <s.reszkojr@gmail.com>
2023-10-02 05:14:45 -0300

    centralized header items

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,11 @@ import './Hero.css';
 const Hero = () => {
 	return (
 		<div className='hero mx-auto'>
-			<div className='aqua-gradient' />
-			<div className='blue-gradient' />
-			<div className='pink-gradient' />
+			<div className='socrates-gradient' />
+			{/* <div className='blue-gradient' /> */}
+			{/* <div className='pink-gradient' /> */}
 			<div className='float-left flex w-full flex-col place-items-center space-y-4 px-20 pt-20 text-center'>
-				<h1 className='w-2/3 font-serif text-6xl font-bold leading-[90px]'>We gathered all the tools required to make you even more potent.</h1>
+				<h1 className='w-2/3 font-serif text-6xl font-bold leading-[90px]'>Manage your classroom with ease and efficiency.</h1>
 				<h2 className='w-[850px] pb-12 text-2xl text-gray'>Manthano is an open-source platform designed to help students and teachers achieve max productivity in a completely intuitive way.</h2>
 				<button className='call-to-btn w-60 rounded-[16px] bg-gradient-to-bl from-teal to-lapis px-3 py-3 font-sans text-2xl font-bold'>START LEARNING</button>
 			</div>
