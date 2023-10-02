4394947a4cd50fe099abe4f22fa5394619639edf
reszkojr <s.reszkojr@gmail.com>
2023-10-02 05:40:22 -0300

    made socrates point to the button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { BsGithub } from 'react-icons/bs';
+
 import './Hero.css';
 
 const Hero = () => {
@@ -7,11 +9,16 @@ const Hero = () => {
 			{/* <div className='blue-gradient' /> */}
 			{/* <div className='pink-gradient' /> */}
 			<div className='float-left flex w-full flex-col place-items-center space-y-4 px-20 pt-20 text-center'>
-				<h1 className='w-2/3 font-serif text-6xl font-bold leading-[90px]'>Manage your classroom with ease and efficiency.</h1>
-				<h2 className='w-[850px] pb-12 text-2xl text-gray'>Manthano is an open-source platform designed to help students and teachers achieve max productivity in a completely intuitive way.</h2>
-				<button className='call-to-btn w-60 rounded-[16px] bg-gradient-to-bl from-teal to-lapis px-3 py-3 font-sans text-2xl font-bold'>START LEARNING</button>
+				<h1 className='w-2/3 text-6xl font-extrabold leading-[90px]'>Manage your classroom with ease and efficiency.</h1>
+				<h2 className='text-gray w-[850px] pb-12 text-2xl'>Manthano is an open-source platform designed to help students and teachers achieve max productivity in a completely intuitive way.</h2>
+				<div className='flex w-1/2 justify-center space-x-24'>
+					<a href='https://github.com/reszkojr/manthano-frontend' target='_blank' className='call-to-btn bg-gray flex w-60 items-center justify-center gap-3 rounded-[16px] bg-gray-800 px-3 py-4 font-sans text-2xl font-bold'>
+						Give us a star! <BsGithub />
+					</a>
+					<button className='call-to-btn w-60 rounded-[16px] bg-gradient-to-bl from-teal-500 to-lapis-500 px-3 py-4 font-sans text-2xl font-bold'>Create an account!</button>
+				</div>
 			</div>
-			<img src='landingpage/socrates4-half-pixelized.png' className='absolute left-1/2 top-[60vh] -z-10 w-[1000px] -translate-x-1/2' alt='the man' />
+			<img src='landingpage/socrates4.png' className='mx-auto mt-24 w-[1000px]' alt='the man' />
 		</div>
 	);
 };
--- src/components/LargeHeader.tsx
+++ src/components/LargeHeader.tsx
@@ -12,10 +12,10 @@ const Header = () => {
 	const redirect = () => navigate('/');
 	return (
 		<header id='header'>
-			<div className='flex items-center px-20 pt-10 text-xl text-gray'>
+			<div className='flex items-center justify-around px-60 pt-10 text-xl text-gray'>
 				<ReactSVG src='/m_manthano.svg' onClick={() => redirect()} className='w-12 cursor-pointer' />
-				<div className='flex-end ml-auto flex space-x-64'>
-					<div className='ml-6 flex space-x-12'>
+				<div className='flex space-x-64'>
+					<div className='flex space-x-12'>
 						<Link className='transition-colors hover:text-white' to='/'>
 							Home
 						</Link>
@@ -29,17 +29,17 @@ const Header = () => {
 							Contribute
 						</Link>
 					</div>
-					<div className='ml-auto'>
-						{user ? (
-							<Link to='/profile'>
-								<div className='flex space-x-2 transition-colors hover:text-white'>
-									<HiUserCircle className='mt-1 h-auto w-6' /> <span>{user.username}</span>
-								</div>
-							</Link>
-						) : (
-							<Link to='/login'>Login</Link>
-						)}
-					</div>
+				</div>
+				<div className=''>
+					{user ? (
+						<Link to='/profile'>
+							<div className='flex space-x-2 transition-colors hover:text-white'>
+								<HiUserCircle className='mt-1 h-auto w-6' /> <span>{user.username}</span>
+							</div>
+						</Link>
+					) : (
+						<Link to='/login'>Login</Link>
+					)}
 				</div>
 			</div>
 		</header>
