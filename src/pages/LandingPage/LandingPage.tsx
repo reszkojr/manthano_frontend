2e4a4ba15f4f66e27da76cd54fbfb8faf04c39ad
Fabio S. Reszko Jr. <67809084+reszkojr@users.noreply.github.com>
2023-10-04 04:41:22 +0000

    removed duplicate header on landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,8 @@
 import Hero from '../../components/landing-page/Hero';
-import LandingPageHeader from '../../components/landing-page/LargeHeader';
 
 const LandingPage = () => {
 	return (
 		<div>
-			<LandingPageHeader />
 			<Hero />
 		</div>
 	);
