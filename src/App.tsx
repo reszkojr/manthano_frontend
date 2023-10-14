f3cc7bb3f0567b0bb91e11d59a85b0fafe83af58
reszkojr <s.reszkojr@gmail.com>
2023-10-13 23:30:47 -0300

    RequireAuth to view JoinClassroom

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ClassroomJoinPage from './pages/classroom/ClassroomJoinPage';
 import LandingPage from './pages/landingpage/LandingPage';
 import LoginPage from './pages/authentication/LoginPage';
 import SignUpPage from './pages/authentication/SignUpPage';
+import RequireAuth from './utils/RequireAuth';
 
 const App = () => {
 	return (
@@ -13,7 +14,14 @@ const App = () => {
 			<Router>
 				<Routes>
 					<Route path='/' element={<LandingPage />} />
-					<Route path='/joinclassroom' element={<ClassroomJoinPage />} />
+					<Route
+						path='/joinclassroom'
+						element={
+							<RequireAuth>
+								<ClassroomJoinPage />
+							</RequireAuth>
+						}
+					/>
 					<Route path='/login' element={<LoginPage />} />
 					<Route path='/signup' element={<SignUpPage />} />
 				</Routes>
--- src/utils/RequireAuth.tsx
+++ src/utils/RequireAuth.tsx
@@ -1,25 +1,19 @@
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 import Props from './Props';
-import { useEffect, useState } from 'react';
 
 const RequireAuth = ({ children }: Props) => {
+	const token = localStorage.getItem('token');
 	const navigate = useNavigate();
-	const [isAuthenticated, setAuthenticated] = useState(false);
-
-	const checkUserToken = () => {
-		const userToken = localStorage.getItem('token');
-		if (!userToken) {
-			setAuthenticated(false);
-			return navigate('/login');
-		}
-		setAuthenticated(true);
-	};
 
 	useEffect(() => {
-		checkUserToken();
-	}, [isAuthenticated]);
+		if (!token) {
+			navigate('/login');
+		}
+	});
 
-	return <>{isAuthenticated ? children : null}</>;
+	return <>{token ? children : null}</>;
 };
 
 export default RequireAuth;
