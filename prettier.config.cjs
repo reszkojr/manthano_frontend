46ad407753cd72c79c8f60b4336febea5c0ff068
reszkojr <s.reszkojr@gmail.com>
2023-10-19 12:46:31 -0300

    prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+
+module.exports = {
+    arrowParens: "always",
+    bracketSpacing: true,
+    endOfLine: "lf",
+    htmlWhitespaceSensitivity: "css",
+    insertPragma: false,
+    singleAttributePerLine: false,
+    bracketSameLine: false,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 10000,
+    proseWrap: "always",
+    quoteProps: "as-needed",
+    requirePragma: false,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "es5",
+    useTabs: true,
+    embeddedLanguageFormatting: "auto",
+    vueIndentScriptAndStyle: false,
+    filepath:
+        "/home/fabiojr2/Langs/JavaScript/manthano_frontend/src/components/classroom/Sidebar.tsx",
+    parser: "typescript",
+    plugins: ["prettier-plugin-tailwindcss"],
+};
