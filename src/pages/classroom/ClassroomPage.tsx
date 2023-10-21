4eaa0cbbedb18826a94fe5c1cf5bebee46928f79
reszkojr <s.reszkojr@gmail.com>
2023-10-21 11:30:03 -0300

    better channels section colors

--- src/pages/classroom/ClassroomPage.tsx
+++ src/pages/classroom/ClassroomPage.tsx
@@ -1,22 +1,28 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import useWebSocket from 'react-use-websocket';
 
 import { useAuth } from '../../components/hooks/UseAuth';
 import Sidebar from '../../components/classroom/Sidebar';
 import NavigationPanel from '../../components/classroom/NavigationPanel';
 import Channel from '../../components/classroom/Channel';
+import { useParams } from 'react-router-dom';
 
 const ClassroomPage = () => {
+	const { classroom_code } = useParams();
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState<string[]>([]);
-	const { token } = useAuth();
+	const { token, tokenCheck } = useAuth();
 
-	const { sendJsonMessage } = useWebSocket(`ws://localhost:8000/ws/info4/?token=${token}`, {
+	const { sendJsonMessage } = useWebSocket(`ws://localhost:8000/ws/${classroom_code}/?token=${token}`, {
+		onError: () => {
+			tokenCheck(token || '');
+		},
 		onMessage: (event: MessageEvent) => {
 			const data = JSON.parse(event.data);
 			const message = `${data['user']}: ${data['message']}`;
 			setMessages([...messages, message]);
 		},
+		shouldReconnect: () => true,
 		reconnectAttempts: 2,
 	});
 
@@ -39,16 +45,6 @@ const ClassroomPage = () => {
 				<Channel />
 			</div>
 		</div>
-		// 		<div className='w-96 h-96 bg-gray-800 rounded-lg m-auto'>
-		// 	<div className='w-full h-full mb-4 p-4'>
-		// 		{messages.map((message, i) => (
-		// 			<div key={i}>{message}</div>
-		// 		))}
-		// 	</div>
-		// 	<form method='POST' onSubmit={handleMessageSubmit}>
-		// 		<TextInputWithButton type='text' value={message} onChange={(e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)} className='flex-1 w-full' name='message' placeholder='Type your message here' buttonLabel='Send' buttonType='submit' />
-		// 	</form>
-		// </div>
 	);
 };
 
