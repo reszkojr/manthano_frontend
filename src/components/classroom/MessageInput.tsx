c4936e4afca082fa390753892f725989e4c6b24e
reszkojr <s.reszkojr@gmail.com>
2023-10-21 11:36:20 -0300

    make messages input expand with longer messages & make scrollbar track lighter

--- src/components/classroom/MessageInput.tsx
+++ src/components/classroom/MessageInput.tsx
@@ -7,13 +7,13 @@ import { AiOutlineGif } from 'react-icons/ai';
 
 const MessageInput = () => {
 	return (
-		<div className='m-4 overflow-hidden rounded-md border border-gray-600 px-2 pt-2'>
-			<div className='max-h0=-28 flex h-8 gap-4'>
-				<div contentEditable={true} spellCheck={false} id='message_input' className='h-8 max-h-28 w-full resize-none bg-transparent p-0 text-gray-50'></div>
+		<div className='m-4 overflow-auto rounded-md border border-gray-600 px-2 pt-2'>
+			<div className='mb-2 flex gap-4'>
+				<div contentEditable={true} spellCheck={false} id='message_input' className='min-h-8 max-h-28 w-full resize-none bg-transparent p-0 text-gray-50'></div>
 				<BiSolidSend className='mb-auto h-auto w-6 hover:cursor-pointer hover:brightness-150 hover:filter' />
 			</div>
 			<div className='h-8'>
-				<ul className='mb-auto flex items-center gap-3 '>
+				<ul className='mb-auto flex items-center gap-3'>
 					<li>
 						<BiImageAdd className='h-auto w-5 hover:cursor-pointer hover:brightness-150 hover:filter' />
 					</li>
