071e56cf9a208a472d7a36dcb7da6966e9a7a774
reszkojr <s.reszkojr@gmail.com>
2023-10-23 07:54:50 -0300

    fix user verification on LargeHeader component & make login and signup redirect to the right place

--- src/components/landing-page/LargeHeader.tsx
+++ src/components/landing-page/LargeHeader.tsx
@@ -11,37 +11,35 @@ const LargeHeader = () => {
 	return (
 		<nav className='border-b border-b-gray-600'>
 			<div className='text-gray flex items-center justify-between p-4 px-12'>
-				<div className='flex space-x-12 items-center'>
+				<div className='flex items-center space-x-12'>
 					<img src='/m_manthano.png' onClick={() => redirect()} className='w-8 cursor-pointer' />
 					<nav className='flex space-x-4 text-gray-200'>
-						<Link className='hover:rounded-md hover:bg-gray-700 px-3 py-2 hover:text-white' to='/'>
+						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/'>
 							Home
 						</Link>
-						<Link className='hover:rounded-md hover:bg-gray-700 px-3 py-2 hover:text-white' to='/classroom'>
+						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/classroom'>
 							Classroom
 						</Link>
-						<Link className='hover:rounded-md hover:bg-gray-700 px-3 py-2 hover:text-white' to='/faq'>
+						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/faq'>
 							FAQ
 						</Link>
-						<Link className='hover:rounded-md hover:bg-gray-700 px-3 py-2 hover:text-white' to='/contribute'>
+						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/contribute'>
 							Contribute
 						</Link>
 					</nav>
 				</div>
 				<>
-					{user!.username ? (
+					{user ? (
 						<Link to='/profile'>
-							<div className='flex space-x-2 items-center hover:rounded-md text-gray-200 hover:bg-gray-700 px-3 py-2 hover:text-white'>
+							<div className='flex items-center space-x-2 px-3 py-2 text-gray-200 hover:rounded-md hover:bg-gray-700 hover:text-white'>
 								<HiUserCircle className='mt-1 h-auto w-5' /> <span>{user!.username}</span>
 							</div>
 						</Link>
 					) : (
-						<div className='space-x-8 text-gray-50'>
-							<Link className='' to='/login'>
-								Login
-							</Link>
-							<Link className='w-60 rounded-[4px] bg-gradient-to-bl from-teal-500 to-lapis-500 px-4 py-2 hover:bg-white' to='/signup'>
-								Sign up for free
+						<div className='flex items-center space-x-8 text-gray-50'>
+							<Link to='/auth/login'>Login</Link>
+							<Link to='/auth/signup'>
+								<div className='rounded-[4px] bg-gradient-to-bl from-teal-500 to-lapis-500 px-4 py-2 hover:bg-white'>Sign up</div>
 							</Link>
 						</div>
 					)}
