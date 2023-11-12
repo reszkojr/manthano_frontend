daf9f1170aedd93a78e2f318b6f3c3c98465cfac
reszkojr <s.reszkojr@gmail.com>
2023-11-12 17:42:04 -0300

    NavigationPanel collapse fix

--- src/components/classroom/NavigationPanel.tsx
+++ src/components/classroom/NavigationPanel.tsx
@@ -16,52 +16,54 @@ const NavigationPanel = () => {
 	};
 
 	return (
-		<div className={classNames('flex h-screen w-56 flex-col gap-3 border-r border-r-gray-600 bg-gray-800 transition-[width] duration-200', { collapsed: isPanelCollapsed })}>
-			<div className='flex h-12 w-full items-center justify-between border-b border-b-gray-600 px-4'>
-				<span className='my-auto min-w-max text-xl font-bold text-gray-50'>Classroom Name</span>
-			</div>
-			<div className='space-y-4 px-3'>
-				<ul className='flex flex-col gap-2'>
-					<li className='flex items-center justify-between'>
-						<div className='flex min-w-max items-center gap-2 text-sm text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter'>
-							CHANNELS <MdExpandMore />
-						</div>
-						<AiOutlinePlus className='text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter' />
-					</li>
-					{classroom.channels.map((channel) => (
-						<li key={channel.id} className={`flex cursor-pointer items-center gap-2 rounded-md px-4 py-[4px] text-gray-200 hover:bg-gray-600 ${classroom.activeChannel?.name === channel.name ? 'bg-gray-700 brightness-150' : ''}`} onClick={() => handleChannelChange(channel.id)}>
-							<FaHashtag className='text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter' />
-							{channel.name}
+		<div className={classNames('h-screen border-r border-r-gray-600 bg-gray-800 transition-[width] duration-200', { collapsed: isPanelCollapsed, 'w-56': !isPanelCollapsed })}>
+			<div className={classNames('flex flex-col gap-3 transition-opacity duration-200', { 'invisible opacity-0': isPanelCollapsed })}>
+				<div className='flex h-12 w-full items-center justify-between border-b border-b-gray-600 px-4'>
+					<span className='my-auto min-w-max text-xl font-bold text-gray-50'>Classroom Name</span>
+				</div>
+				<div className='space-y-4 px-3'>
+					<ul className='flex flex-col gap-2'>
+						<li className='flex min-w-max items-center justify-between'>
+							<div className='flex min-w-max items-center gap-2 text-sm text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter'>
+								CHANNELS <MdExpandMore />
+							</div>
+							<AiOutlinePlus className='text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter' />
 						</li>
-					))}
-				</ul>
-				<div className='flex flex-col gap-2'>
-					<div className='flex items-center justify-between'>
-						<div className='flex min-w-max items-center gap-2 text-sm text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter'>
-							VOICE CHANNELS <MdExpandMore />
+						{classroom.channels.map((channel) => (
+							<li key={channel.id} className={`flex min-w-max cursor-pointer items-center gap-2 rounded-md px-4 py-[4px] text-gray-200 hover:bg-gray-600 ${classroom.activeChannel?.name === channel.name ? 'bg-gray-700 brightness-150' : ''}`} onClick={() => handleChannelChange(channel.id)}>
+								<FaHashtag className='text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter' />
+								{channel.name}
+							</li>
+						))}
+					</ul>
+					<div className='flex flex-col gap-2'>
+						<div className='flex items-center justify-between'>
+							<div className='flex min-w-max items-center gap-2 text-sm text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter'>
+								VOICE CHANNELS <MdExpandMore />
+							</div>
+							<AiOutlinePlus className='text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter' />
 						</div>
-						<AiOutlinePlus className='text-gray-300 hover:cursor-pointer hover:brightness-150 hover:filter' />
-					</div>
-					<div className='text-md ml-4 flex min-w-max flex-col gap-1 text-lg'>
-						<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
-							<FaVolumeDown className='h-auto w-4' />
-							math
-						</div>
-						<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
-							<FaVolumeDown className='h-auto w-4' />
-							science
-						</div>
-						<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
-							<FaVolumeDown className='h-auto w-4' />
-							geography
-						</div>
-						<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
-							<FaVolumeDown className='h-auto w-4' />
-							chemistry
-						</div>
-						<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
-							<FaVolumeDown className='h-auto w-4' />
-							english
+						<div className='text-md ml-4 flex min-w-max flex-col gap-1 text-lg'>
+							<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
+								<FaVolumeDown className='h-auto w-4' />
+								math
+							</div>
+							<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
+								<FaVolumeDown className='h-auto w-4' />
+								science
+							</div>
+							<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
+								<FaVolumeDown className='h-auto w-4' />
+								geography
+							</div>
+							<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
+								<FaVolumeDown className='h-auto w-4' />
+								chemistry
+							</div>
+							<div className='flex cursor-pointer items-center gap-2 rounded-md px-4 py-[2px] text-gray-200 hover:bg-gray-600'>
+								<FaVolumeDown className='h-auto w-4' />
+								english
+							</div>
 						</div>
 					</div>
 				</div>
