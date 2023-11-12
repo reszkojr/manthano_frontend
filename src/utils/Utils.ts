8ebd307749f2598453853e6d37160eaed8c6a983
reszkojr <s.reszkojr@gmail.com>
2023-11-12 16:36:30 -0300

    Utils file fix

---
+++ src/utils/Utils.ts
@@ -0,0 +1,22 @@
+import { Token } from "../types/Types";
+import jwt_decode from 'jwt-decode';
+
+export const storeTokens = (token: string, refreshToken: string) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('refreshToken', refreshToken);
+};
+
+export const removeTokens = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+};
+
+export const usernameFromToken = (token: string) => {
+    const decodedToken: Token = jwt_decode(token);
+    return decodedToken.username;
+};
+
+export const userIdFromToken = (token: string) => {
+    const decodedToken: Token = jwt_decode(token);
+    return decodedToken.user_id;
+};
