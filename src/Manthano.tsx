a865d45be6e763604b0ed48c805c36e786942c2e
reszkojr <s.reszkojr@gmail.com>
2023-11-12 15:26:12 -0300

    separate main.tsx and Manthano.tsx files

---
+++ src/Manthano.tsx
@@ -0,0 +1,51 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { Slide, ToastContainer } from 'react-toastify';
+import useApi from './hooks/useApi';
+import Root from './components/Root';
+import LandingPage from './pages/landingpage/LandingPage';
+import LoginPage from './pages/authentication/LoginPage';
+import SignUpPage from './pages/authentication/SignUpPage';
+import RequireAuth from './utils/RequireAuth';
+import ClassroomJoinPage from './pages/classroom/ClassroomJoinPage';
+import { ClassroomProvider } from './context/ClassroomContext';
+import ClassroomPage from './pages/classroom/ClassroomPage';
+import ClassroomChannel from './components/classroom/ClassroomChannel';
+import ErrorPage from './pages/ErrorPage';
+
+import '../index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Manthano = () => {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path='/' Component={Root}>
+				<Route index Component={LandingPage} />
+				<Route path='auth'>
+					<Route path='login' Component={LoginPage} />
+					<Route path='signup' Component={SignUpPage} />
+				</Route>
+				<Route Component={RequireAuth}>
+					<Route path='join' Component={ClassroomJoinPage} />
+					<Route path='classroom' Component={ClassroomProvider}>
+						<Route path=':classroom_code' Component={ClassroomPage}>
+							<Route path=':channel_code' Component={ClassroomChannel} />
+						</Route>
+					</Route>
+				</Route>
+				<Route path='*' Component={ErrorPage} />
+			</Route>
+		)
+	);
+
+	return (
+		<>
+			<AuthProvider>
+				<RouterProvider router={router} />
+			</AuthProvider>
+			<ToastContainer position='top-center' transition={Slide} autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable={false} pauseOnHover theme='dark' />
+		</>
+	);
+};
+
+export default Manthano;
--- src/main.tsx
+++ src/main.tsx
@@ -1,51 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.tsx';
-
-import LoginPage from './pages/authentication/LoginPage.tsx';
-import LandingPage from './pages/landingpage/LandingPage.tsx';
-import SignUpPage from './pages/authentication/SignUpPage.tsx';
-import Root from './components/Root.tsx';
-
-import '../index.css';
-import 'react-toastify/dist/ReactToastify.css';
-
-import { Slide, ToastContainer } from 'react-toastify';
-import ClassroomJoinPage from './pages/classroom/ClassroomJoinPage.tsx';
-import ClassroomPage from './pages/classroom/ClassroomPage.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import ClassroomChannel from './components/classroom/ClassroomChannel.tsx';
-import { ClassroomProvider } from './context/ClassroomContext.tsx';
-import RequireAuth from './utils/RequireAuth.tsx';
-
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path='/' Component={Root}>
-			<Route index Component={LandingPage} />
-			<Route path='auth'>
-				<Route path='login' Component={LoginPage} />
-				<Route path='signup' Component={SignUpPage} />
-			</Route>
-			<Route Component={RequireAuth}>
-				<Route path='classroom' Component={ClassroomProvider}>
-					<Route path='join' Component={ClassroomJoinPage} />
-					<Route path=':classroom_code' Component={ClassroomPage}>
-						<Route path=':channel_code' Component={ClassroomChannel} />
-					</Route>
-				</Route>
-			</Route>
-			<Route path='*' Component={ErrorPage} />
-		</Route>
-	)
-);
+import Manthano from './Manthano.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<AuthProvider>
-			<RouterProvider router={router} />
-		</AuthProvider>
-		<ToastContainer position='top-center' transition={Slide} autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable={false} pauseOnHover theme='dark' />
+		<Manthano />
 	</React.StrictMode>
 );
