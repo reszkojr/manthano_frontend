8a50e053fbf24926b401ee1b1b0250a0ab8eb9e4
reszkojr <s.reszkojr@gmail.com>
2023-11-16 20:48:55 -0300

    fix message keys undefined and refactored user message logic

--- src/components/classroom/ChannelChat.tsx
+++ src/components/classroom/ChannelChat.tsx
@@ -20,7 +20,7 @@ const ChannelChat = () => {
 	};
 
 	return (
-		<div className='mb-4 min-w-max flex-1 overflow-y-auto overflow-x-hidden p-4'>
+		<div className='mb-4 min-w-max flex-1 gap-0 overflow-y-auto overflow-x-hidden p-4'>
 			<ul className='h-full'>
 				{loading && (
 					<>
@@ -69,43 +69,36 @@ const ChannelChat = () => {
 					</>
 				)}
 				{messages &&
-					(messages?.length > 0 ? (
-						messages?.map((message, index, msgs) => (
-							<>
-								{msgs[index - 1] !== undefined ? (
-									message.user === msgs[index - 1].user ? (
-										<li key={message.id} className='mb-1 flex'>
-											<div className='ml-14 w-11/12'>
-												<div className='whitespace-pre-line'>{message.text}</div>
-											</div>
-										</li>
-									) : (
-										<li key={message.id} className='mt-4 flex'>
-											<div className='mr-4 mt-2 h-10 w-10 overflow-hidden rounded-full'>
-												<img src={message.avatar} alt='pp' className='h-10 w-10 object-cover' />
-											</div>
-											<div className='w-11/12'>
-												<div className='flex items-center gap-2'>
-													<div className='font-semibold'>{message.user}</div>
-													<span className='text-sm text-gray-400'>{`${message.date}`}</span>
-												</div>
-												<div className='whitespace-pre-line'>{message.text}</div>
-											</div>
-										</li>
-									)
-								) : (
-									<li key={message.id} className='flex'>
-										<div className='mr-4 mt-2 h-10 w-10 overflow-hidden rounded-full'>
+					(messages.length > 0 ? (
+						messages.map((message, index, msgs) => {
+							let content = null;
+							if (msgs[index - 1] !== undefined && msgs[index - 1].user === message.user) {
+								content = (
+									<li key={index} className='flex gap-4'>
+										<div className='w-10'></div>
+										<div className='w-11/12'>
+											<div className='my-[2px] whitespace-pre-line'>{message.text}</div>
+										</div>
+									</li>
+								);
+							} else {
+								content = (
+									<li key={index} className='mt-4 flex gap-4'>
+										<div className='mt-2 h-10 w-10 overflow-hidden rounded-full'>
 											<img src={message.avatar} alt='pp' className='h-10 w-10 object-cover' />
 										</div>
 										<div className='w-11/12'>
-											<div className='font-semibold'>{message.user}</div>
+											<div className='flex items-center gap-2'>
+												<div className='font-semibold'>{message.user}</div>
+												<span className='text-sm text-gray-400'>{`${message.date}`}</span>
+											</div>
 											<div className='whitespace-pre-line'>{message.text}</div>
 										</div>
 									</li>
-								)}
-							</>
-						))
+								);
+							}
+							return content;
+						})
 					) : (
 						<div className='flex h-full select-none items-center justify-center'>
 							<div className='my-auto text-center text-2xl text-gray-400'>No messages here!</div>
