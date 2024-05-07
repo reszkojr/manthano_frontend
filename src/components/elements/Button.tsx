a0c9aa7df0be825636dcc70351d2d30242a6f99a
reszkojr <s.reszkojr@gmail.com>
2024-05-07 17:46:46 -0300

    pass icon as prop to Button and remove outline

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,6 +1,7 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react';
 
 type ButtonProps = {
+	icon?: React.ReactNode;
 	label: string;
 	className?: string;
 	onClick: MouseEventHandler<HTMLButtonElement> | undefined;
@@ -8,10 +9,11 @@ type ButtonProps = {
 
 const Button = (props: ButtonProps) => {
 	const styles = {
-		Button: 'text-white bg-gray-600 hover:bg-lapis-500 transition-colors duration-100 focus:ring-4 focus:outline-none focus:ring-teal-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center ' + props.className,
+		Button: 'flex gap-2 items-center text-white bg-gray-600 hover:bg-lapis-500 transition-colors duration-100 focus:outline-none focus:ring-teal-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center ' + props.className,
 	};
 	return (
 		<button type='button' onClick={props.onClick} className={styles.Button}>
+			{props.icon && <div>{props.icon}</div>}
 			{props.label}
 		</button>
 	);
