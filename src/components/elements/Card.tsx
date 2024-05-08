8b51c180dff8ab2c9a191461a74b5c035e6e1844
reszkojr <s.reszkojr@gmail.com>
2024-05-08 19:29:07 -0300

    allow Card component to be rendered without an icon

--- src/components/elements/Card.tsx
+++ src/components/elements/Card.tsx
@@ -1,23 +1,21 @@
 interface CardProps {
 	title: string;
 	icon?: React.ReactNode;
-	href: string;
+	onClick?: React.MouseEventHandler<HTMLDivElement> | undefined;
 	subtitle?: string;
 	className?: string;
 }
 
 const Card = (props: CardProps) => {
-	const styles = { card: `bg-gray-700 shadow-md rounded-md p-3 hover:filter hover:brightness-125 cursor-pointer transition-all duration-75 ${props.className}` };
+	const styles = { card: `flex gap-3 bg-gray-700 shadow-md rounded-md p-3 hover:filter hover:brightness-125 cursor-pointer transition-all duration-75 ${props.className}` };
 
 	return (
-		<div className={styles.card}>
-			<a href={props.href} className='flex'>
-				{props.icon && <div className='my-auto ml-2 mr-6'>{props.icon}</div>}
-				<div className='content-center pr-2'>
-					<h2 className='text-md font-semibold'>{props.title}</h2>
-					{props.subtitle && <p className='text-md text-gray-300'>{props.subtitle}</p>}
-				</div>
-			</a>
+		<div onClick={props.onClick} className={styles.card}>
+			{props.icon && <div>{props.icon}</div>}
+			<div className='content-center pr-2'>
+				<h2 className='text-md font-semibold text-gray-100'>{props.title}</h2>
+				{props.subtitle && <p className='text-md text-gray-300'>{props.subtitle}</p>}
+			</div>
 		</div>
 	);
 };
