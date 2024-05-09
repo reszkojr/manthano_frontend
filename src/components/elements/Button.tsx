eeb842632462f63c0ed10e594d70e667de14a97c
reszkojr <s.reszkojr@gmail.com>
2024-05-09 16:59:43 -0300

    disabled Prop on Button

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -4,6 +4,7 @@ type ButtonProps = {
 	icon?: React.ReactNode;
 	label: string;
 	className?: string;
+	disabled?: boolean;
 	onClick: MouseEventHandler<HTMLButtonElement> | undefined;
 };
 
@@ -12,7 +13,7 @@ const Button = (props: ButtonProps) => {
 		Button: 'flex gap-2 items-center text-white bg-gray-600 hover:bg-lapis-500 transition-colors duration-100 focus:outline-none focus:ring-teal-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center ' + props.className,
 	};
 	return (
-		<button type='button' onClick={props.onClick} className={styles.Button}>
+		<button type='button' disabled={props.disabled} onClick={props.onClick} className={styles.Button}>
 			{props.icon && <div>{props.icon}</div>}
 			{props.label}
 		</button>
