adaf2445846fe373ef46724b6c41d7fd64c0c9cd
reszkojr <s.reszkojr@gmail.com>
2024-08-03 13:49:57 -0300

    redirect to /join instead of /classroom

--- src/components/landing-page/LargeHeader.tsx
+++ src/components/landing-page/LargeHeader.tsx
@@ -12,12 +12,12 @@ const LargeHeader = () => {
 		<nav className='border-b border-b-gray-700'>
 			<div className='text-gray flex items-center justify-between p-4 px-12'>
 				<div className='flex items-center space-x-12'>
-					<img src='/m_manthano.png' onClick={() => redirect()} className='w-8 cursor-pointer' />
+					<img src='/m_manthano.png' alt={'manthano_logo'} onClick={() => redirect()} className='w-8 cursor-pointer' />
 					<nav className='flex space-x-4 text-gray-200'>
 						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/'>
 							Home
 						</Link>
-						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/classroom'>
+						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/join'>
 							Classroom
 						</Link>
 						<Link className='px-3 py-2 hover:rounded-md hover:bg-gray-700 hover:text-white' to='/faq'>
